07dbf1a4cdae45df13d599e2dd016825f294371a
ElenaGlu <shishalovae@mail.ru>
2024-01-09 19:06:53 +0500

    ok

---
+++ asteroids.py
@@ -0,0 +1,36 @@
+import asyncio
+import aiohttp
+import datetime
+
+API_KEY = 'an6PHxhDVYuw17CTWGvypLcLGq3DLhGG0vdeup81'
+start_dates = []
+
+date1 = '2023-09-01'
+date2 = '2023-10-05'
+start = datetime.datetime.strptime(date1, '%Y-%m-%d')
+end = datetime.datetime.strptime(date2, '%Y-%m-%d')
+step = datetime.timedelta(days=7)
+while start <= end:
+    start_dates.append(start)
+    start += step
+
+
+async def fetch(session, url):
+    async with session.get(url) as resp:
+        # print(resp.status)
+        return (await resp.json())['element_count']
+
+
+async def collect():
+    async with aiohttp.ClientSession() as session:
+        fetch_awaitables = [
+            fetch(session,
+                  f'https://api.nasa.gov/neo/rest/v1/feed?start_date='
+                  f'{start_date}&end_date={start_date + datetime.timedelta(days=6)}&api_key={API_KEY}')
+            for start_date in start_dates
+        ]
+        return sum(await asyncio.gather(*fetch_awaitables))
+
+currencies = asyncio.run(collect())  # 853 asteroids
+# print(currencies)
+# print(json.dumps(currencies, indent=4, ensure_ascii=False))
